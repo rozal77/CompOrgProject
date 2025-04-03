1f824f2fbcc222cbeed5d320ee0d1ea3459ddec1
rozal77 <uniquerojal@gmail.com>
2025-04-02 20:33:45 -0400

    Added ANSI escape codes for terminal color formatting

---
+++ floatingbenchmark.py
@@ -0,0 +1,10 @@
+import time
+ 
+ # ANSI escape codes for colors
+ ANSI_COLOR_RED = "\x1b[31m"
+ ANSI_COLOR_GREEN = "\x1b[32m"
+ ANSI_COLOR_YELLOW = "\x1b[33m"
+ ANSI_COLOR_RESET = "\x1b[0m"
+ 
+ def main():
+     total_time = 0
