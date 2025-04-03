24d5c8adcd0e1b3266a840edb889c230a876313f
rozal77 <uniquerojal@gmail.com>
2025-04-02 20:53:15 -0400

    Initialized file size parameters for disk benchmarking

---
+++ harddrivebenchmark2.py
@@ -0,0 +1,12 @@
+import os
+ import time
+ 
+ # ANSI escape codes for colors
+ ANSI_COLOR_RED = "\x1b[31m"
+ ANSI_COLOR_GREEN = "\x1b[32m"
+ ANSI_COLOR_YELLOW = "\x1b[33m"
+ ANSI_COLOR_RESET = "\x1b[0m"
+ 
+ def main():
+     total_file_size = 1000000000
+     one_time_size = 10000
