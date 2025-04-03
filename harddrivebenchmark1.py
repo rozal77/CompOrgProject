06a04d30356d9a485a4eda03a0086ce394ac3f65
rozal77 <uniquerojal@gmail.com>
2025-04-02 20:48:57 -0400

    Implemented disk write benchmarking

--- harddrivebenchmark1.py
+++ harddrivebenchmark1.py
@@ -10,3 +10,17 @@ import os
  def main():
      total_file_size = 1000000000
      one_time_size = 100
+     total_time = 0
+ 
+     # Write 10^9 bytes to a file, 100 bytes each time
+     with open("dummyOneBillionBytes.txt", "wb") as output_file_handler:
+         start = time.time()
+         output_buffer = bytearray(b'A' * one_time_size)
+         bytes_written_so_far = 0
+ 
+         while bytes_written_so_far < total_file_size:
+             output_file_handler.write(output_buffer)
+             bytes_written_so_far += one_time_size
+ 
+         end = time.time()
+         total_time = end - start
